e6d8273b7116d16724115779908408336347ab8e
hunterjreid <62681404+hunterjreid@users.noreply.github.com>
2022-10-04 09:51:14 +1300

    Network Scanner functionality with basic GUI 😊
    TODO NEXT: 
    
    - visualize data in graph
    - expand upon GUI

--- README.md
+++ README.md
@@ -5,6 +5,7 @@
 Created with PyQt5 & psutil
 
 Vulnerability scanner for home networks built in Python aided by Wireshark
+![image](https://user-images.githubusercontent.com/62681404/193679701-800ac938-65a4-4c90-8617-e524c2b82efa.png)
 ![eqweeqwqweqwe](https://user-images.githubusercontent.com/62681404/193159632-6fbdce7b-8cc2-435d-8337-ac00bd9b8220.png)
 ![MicrosoftTeams-image (6)](https://user-images.githubusercontent.com/62681404/193483972-18fff5bc-a413-44e8-805a-00c00803cf78.png)
 
--- main.py
+++ main.py
@@ -1,41 +1,58 @@
-import sys
-import psutil
-import time
 from PyQt5 import QtWidgets, uic
+import psutil
 
-app = QtWidgets.QApplication(sys.argv)
+app = QtWidgets.QApplication([])
+dlg = uic.loadUi("mainwindow.ui")
 
 
+def btn1():
+    dlg.textEdit.append(str(psutil.net_io_counters(pernic=True)))
+def btn9():
+    dlg.textEdit.clear()
 
 
-def my_function():
-    
+def btn2():
     last_received = psutil.net_io_counters().bytes_recv
     last_sent = psutil.net_io_counters().bytes_sent
     last_total = last_received + last_sent
-
-    bytes_received = psutil.net_io_counters().bytes_recv
-    bytes_sent = psutil.net_io_counters().bytes_sent
-    bytes_total = bytes_received + bytes_sent
-
-    new_received = bytes_received - last_received
-    new_sent =  bytes_sent - last_sent
-    new_total = bytes_total - last_total
-
-    mb_new_received = new_received / 1024 / 1024
-    mb_new_sent = new_sent / 1024 / 1024
-    mb_new_total = new_total / 1024 / 1024
-
-    print(f"{mb_new_received:.2f} MB received, {mb_new_sent:.2f} MB sent, {mb_new_total:.2f} MB total.")
-
-    last_received = bytes_received
-    last_sent = bytes_sent
-    last_total = bytes_total
-
-    time.sleep(1)
+    mb_new_received = last_received / 1024 / 1024
+    mb_new_sent = last_sent / 1024 / 1024
+    mb_new_total = last_total / 1024 / 1024
+    dlg.textEdit.append(f"{mb_new_received:.2f} MB received, {mb_new_sent:.2f} MB sent, {mb_new_total:.2f} MB total.")
 
 
-window = uic.loadUi("mainwindow.ui")
-window.show()
-my_function()
+def btn3():
+    dlg.textEdit.append(str(psutil.net_connections()))
+
+def btn4():
+    dlg.textEdit.append(str(psutil.net_if_stats()))
+
+def btn14():
+
+
+    dlg.textEdit.append(str(psutil.net_connections(kind="tcp")))
+   
+
+
+def btn8():
+    dlg.textEdit.append(str(psutil.net_if_addrs()))
+def btn5():
+    dlg.textEdit.append(str(psutil.net_connections(kind='inet')))
+
+
+def btn7():
+    dlg.textEdit.append(str(psutil.net_if_addrs()))
+
+
+dlg.pushButton.clicked.connect(btn1)
+dlg.pushButton_2.clicked.connect(btn2)
+dlg.pushButton_3.clicked.connect(btn4)
+dlg.pushButton_4.clicked.connect(btn3)
+dlg.pushButton_9.clicked.connect(btn9)
+dlg.pushButton_8.clicked.connect(btn8)
+dlg.pushButton_5.clicked.connect(btn5)
+dlg.pushButton_6.clicked.connect(btn14)
+dlg.pushButton_7.clicked.connect(btn7)
+
+dlg.show()
 app.exec()
