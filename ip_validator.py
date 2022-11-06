e4ebcdd38609e61445c7e5d1541d2b8259ee8593
hunterjreid <62681404+hunterjreid@users.noreply.github.com>
2022-11-06 15:59:35 +1300

    Minor

---
+++ ip_validator.py
@@ -0,0 +1,16 @@
+import os, ipaddress 
+ 
+os.system('cls')
+ 
+while True:
+    ip = input('Enter IP Address: ')
+    try: 
+        print(ipaddress.ip_address(ip))
+        print('IP Valid')
+    except: 
+        print('-' *50)
+        print('IP is not valid')
+    finally: 
+        if ip =='q':
+           print('Script Finished')
+           break
